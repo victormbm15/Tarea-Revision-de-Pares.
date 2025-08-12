dbffa4c096c28773f07013586ccd82757ff96982
Victor Manuel <vijobero139@gmail.com>
2025-08-12 12:16:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { store } from './Store';
 import HomePage from './components/HomePage';
 import ProductsPage from './components/ProductsPage';
 import CartPage from './components/CartPage';
